a5369fbd76fc6425d7f34eda2cd22af00976f2e9
Вагиз <simvagiz@gmail.com>
2026-05-15 23:03:49 +0300

    fix bag v2 UCHYOBA-10

--- src/main/java/com/academy/zug_zug/Main.java
+++ src/main/java/com/academy/zug_zug/Main.java
@@ -1,18 +1,6 @@
 package com.academy.zug_zug;
 import java.util.*;
 
-class Animal{
-    private int i;
-}
-
-class Mouse extends Animal{
-
-}
-class Dog extends Animal{
-
-}
-
-
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> cats = new ArrayList<>();
