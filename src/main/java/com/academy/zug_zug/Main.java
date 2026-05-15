27f668b31a40e70a6eb3c6355263f6fda5d982f7
Вагиз <simvagiz@gmail.com>
2026-05-15 22:47:09 +0300

    Commit and push testing

--- src/main/java/com/academy/zug_zug/Main.java
+++ src/main/java/com/academy/zug_zug/Main.java
@@ -1,8 +1,27 @@
 package com.academy.zug_zug;
+import java.util.*;
+
+class Animal{
+    private int i;
+}
+
+class Mouse extends Animal{
+
+}
+class Dog extends Animal{
+
+}
 
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> cats = new ArrayList<>();
         System.out.println("hello, bobr kurwa");
+        cats.add("Hey boy, nice haircut*  *");
+        cats.add("                      __");
+        System.out.println("Kuku");
+        System.out.println(cats.get(0));
+        System.out.println(cats.get(1));
+
     }
 }
