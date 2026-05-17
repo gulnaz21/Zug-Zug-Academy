724a0cc273c496f50059036b55e2de2da1a35d5f
Вагиз <simvagiz@gmail.com>
2026-05-17 22:26:57 +0300

    commit #2

--- src/main/java/com/academy/zug_zug/Main.java
+++ src/main/java/com/academy/zug_zug/Main.java
@@ -1,9 +1,11 @@
 package com.academy.zug_zug;
-
+import java.util.Random;
+import java.util.random.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello, bobr kurwa1");
         System.out.println("privet soset");
+        System.out.println(new Random().nextLong());
     }
 }
