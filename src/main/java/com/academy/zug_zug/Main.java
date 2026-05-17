a83796858e7f27c5bf89b3240c1324a276861d26
Вагиз <simvagiz@gmail.com>
2026-05-17 22:24:25 +0300

    commit #1

--- src/main/java/com/academy/zug_zug/Main.java
+++ src/main/java/com/academy/zug_zug/Main.java
@@ -4,5 +4,6 @@ package com.academy.zug_zug;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello, bobr kurwa1");
+        System.out.println("privet soset");
     }
 }
