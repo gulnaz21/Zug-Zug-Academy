2c1bb9e81a2b771609f3f6a744ce0226c3e4e870
Вагиз <simvagiz@gmail.com>
2026-05-21 20:28:48 +0300

    Homework№15 commit#1

---
+++ src/main/java/com/academy/zug_zug/Homework/ep15/LoginTest.java
@@ -0,0 +1,8 @@
+package com.academy.zug_zug.Homework.ep15;
+
+public class LoginTest implements TestScenario{
+    @Override
+    public void runTest() {
+        System.out.println("LoginTest is started");
+    }
+}
---
+++ src/main/java/com/academy/zug_zug/Homework/ep15/MainClass.java
@@ -0,0 +1,13 @@
+package com.academy.zug_zug.Homework.ep15;
+
+interface TestScenario{
+    void runTest() ;
+}
+
+public class MainClass {
+    public static void main(String[] args) {
+        TestScenario [] objects = new TestScenario[]{new LoginTest(),new PaymentTest(),new ProductSearchTest()};
+        for(int i = 0;i <= objects.length - 1; i++)
+            objects[i].runTest();
+    }
+}
