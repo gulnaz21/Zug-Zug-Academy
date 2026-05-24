020527d713e0dc02ab716a3dc6aaf2a3f305ddf7
Вагиз <simvagiz@gmail.com>
2026-05-24 19:50:15 +0300

    UCHYOBA-15-InterFAKE

---
+++ src/main/java/com/academy/zug_zug/Homework/ep15/PaymentTest.java
@@ -0,0 +1,8 @@
+package com.academy.zug_zug.Homework.ep15;
+
+public class PaymentTest implements TestScenario {
+    @Override
+    public void runTest() {
+        System.out.println("PaymentTest is started");
+    }
+}
