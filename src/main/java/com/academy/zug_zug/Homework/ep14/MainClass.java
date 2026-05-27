5e82db48db67e1b78c518d95eb1da0dd4c271e38
Вагиз <simvagiz@gmail.com>
2026-05-27 12:43:10 +0300

    UCHYOBA-15-bugFix #2

--- src/main/java/com/academy/zug_zug/Homework/ep14/MainClass.java
+++ src/main/java/com/academy/zug_zug/Homework/ep14/MainClass.java
@@ -8,7 +8,7 @@ public class MainClass {
     private static final int IOT_DEVICE_ID = 5;
     private static final int SMART_LIGHT_ID = 6;
     private static final int SMART_THERMOSTAT_ID = 7;
-    private static final int smartThermostatTemp = 67;
+    private static final int SMART_THERMOSTAT_TEMP = 67;
     private static final int TEMPERATURE_FOR_SET = 52;
     private static final String SOME_DEVICE = "someDevice";
     private static final String SMART_LIGHT = "smartLight";
@@ -17,7 +17,7 @@ public class MainClass {
     public static void main(String[] args) {
         IoTDevice ioTDevice = new IoTDevice(IOT_DEVICE_ID, SOME_DEVICE);
         SmartLight smartLight = new SmartLight(SMART_LIGHT_ID, SMART_LIGHT, false);
-        SmartThermostat smartThermostat = new SmartThermostat(SMART_THERMOSTAT_ID, SMART_THERMOSTAT_NAME, smartThermostatTemp);
+        SmartThermostat smartThermostat = new SmartThermostat(SMART_THERMOSTAT_ID, SMART_THERMOSTAT_NAME, SMART_THERMOSTAT_TEMP);
         ioTDevice.performAction();
         smartLight.performAction();
         smartThermostat.performAction();
