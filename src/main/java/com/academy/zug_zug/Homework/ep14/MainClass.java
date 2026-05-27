1cc2a4e13f982c91fb9b5c80a9a5e696029823d5
Вагиз <simvagiz@gmail.com>
2026-05-27 12:30:10 +0300

    UCHYOBA-15-bugFix #2

--- src/main/java/com/academy/zug_zug/Homework/ep14/MainClass.java
+++ src/main/java/com/academy/zug_zug/Homework/ep14/MainClass.java
@@ -5,23 +5,23 @@ import com.academy.zug_zug.Homework.ep14.SmartDevices.SmartLight;
 import com.academy.zug_zug.Homework.ep14.SmartDevices.SmartThermostat;
 
 public class MainClass {
-    private static final int ioTDeviceId = 5;
-    private static final int smartLightId = 6;
-    private static final int smartThermostatId = 7;
+    private static final int IOT_DEVICE_ID = 5;
+    private static final int SMART_LIGHT_ID = 6;
+    private static final int SMART_THERMOSTAT_ID = 7;
     private static final int smartThermostatTemp = 67;
-    private static final int temperatureForSet = 52;
-    private static final String iotDeviceName = "someDevice";
-    private static final String smartLightName = "smartLight";
-    private static final String smartThermostatName = "ThermostatSixSeven";
+    private static final int TEMPERATURE_FOR_SET = 52;
+    private static final String SOME_DEVICE = "someDevice";
+    private static final String SMART_LIGHT = "smartLight";
+    private static final String SMART_THERMOSTAT_NAME = "ThermostatSixSeven";
 
     public static void main(String[] args) {
-        IoTDevice ioTDevice = new IoTDevice(ioTDeviceId, iotDeviceName);
-        SmartLight smartLight = new SmartLight(smartLightId, smartLightName, false);
-        SmartThermostat smartThermostat = new SmartThermostat(smartThermostatId, smartThermostatName, smartThermostatTemp);
+        IoTDevice ioTDevice = new IoTDevice(IOT_DEVICE_ID, SOME_DEVICE);
+        SmartLight smartLight = new SmartLight(SMART_LIGHT_ID, SMART_LIGHT, false);
+        SmartThermostat smartThermostat = new SmartThermostat(SMART_THERMOSTAT_ID, SMART_THERMOSTAT_NAME, smartThermostatTemp);
         ioTDevice.performAction();
         smartLight.performAction();
         smartThermostat.performAction();
-        smartThermostat.setTemperature(temperatureForSet);
+        smartThermostat.setTemperature(TEMPERATURE_FOR_SET);
         smartThermostat.performAction();
     }
 }
